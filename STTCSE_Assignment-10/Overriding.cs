8667c4d035313f93ccf8d0278f9f569e22528e05
Abhinav-Khot <23110006@iitgn.ac.in>
2025-11-07 15:44:54 +0530

    Add project files.

---
+++ STTCSE_Assignment-10/Overriding.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+public class Vehicle
+{
+    protected int speed;
+    protected int fuel;
+
+    public Vehicle(int s, int f)
+    {
+        speed = s;
+        fuel = f;
+    }
+
+    public virtual void ShowInfo()
+    {
+        Console.WriteLine("Vehicle Speed: " + speed);
+        Console.WriteLine("Vehicle Fuel: " + fuel);
+    }
+
+    public virtual void Drive()
+    {
+        fuel -= 5;
+        Console.WriteLine("Vehicle is moving...");
+    }
+}
+
+public class Car : Vehicle
+{
+    private int passengers;
+
+    public Car(int s, int f, int p) : base(s, f)
+    {
+        passengers = p;
+    }
+
+    public override void ShowInfo()
+    {
+        base.ShowInfo();
+        Console.WriteLine("Car Passenger count: " + passengers);
+    }
+
+    public override void Drive()
+    {
+        fuel -= 10;
+        Console.WriteLine("Car is moving with passenger");
+    }
+}
+
+public class Truck : Vehicle
+{
+    private int cargoWeight;
+
+    public Truck(int s, int f, int c) : base(s, f)
+    {
+        cargoWeight = c;
+    }
+
+    public override void ShowInfo()
+    {
+        base.ShowInfo();
+        Console.WriteLine("Cargo weight: " + cargoWeight);
+    }
+
+    public override void Drive()
+    {
+        fuel -= 10;
+        Console.WriteLine("Truck is moving with cargo");
+    }
+}
+
+public class Overriding
+{
+    public static void Main(string[] args)
+    {
+        Vehicle[] vehicles = {
+            new Vehicle(100, 50),
+            new Car(120, 60, 4),
+            new Truck(80, 70, 1000)
+        };
+
+        for (int i = 0; i < 3; i++)
+        {
+            vehicles[i].ShowInfo();
+            vehicles[i].Drive();
+            Console.WriteLine();
+        }
+    }
+}
---
+++ STTCSE_Assignment-10/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+public class Program
+{
+    private int data;
+    private static int alive = 0;
+    public Program()
+    {
+        System.Console.WriteLine("Constructor Called");
+        alive++;
+        System.Console.WriteLine("Alive Objects: " + alive);
+    }
+
+    public void set_data(int x)
+    {
+        this.data = x;
+    }
+
+    public void show_data()
+    {
+        System.Console.WriteLine("Data: " + this.data);
+    }
+    ~Program()
+    {
+        System.Console.WriteLine("Object with value " + this.data + " Destroyed");
+        alive--;
+        System.Console.WriteLine("Alive Objects: " + alive);
+    }
+
+    public static void Test()
+    {
+        Program p1 = new Program();
+        p1.set_data(10);
+        Program p2 = new Program();
+        p2.set_data(20);
+        Program p3 = new Program();
+        p3.set_data(30);
+
+        p1.show_data();
+        p2.show_data();
+        p3.show_data();
+    }
+
+    public static void Main(string[] args)
+    {
+        Test();
+        GC.Collect();
+        GC.WaitForPendingFinalizers();
+    }
+}
